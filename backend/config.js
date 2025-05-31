a5e11d845e33ae3f18f6cdeb012a4634ba3bc743
varunquanth0001 <varunquanth0001@gmail.com>
2025-05-31 13:46:45 +0530

    first commit

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://sourav7889509712:eReSIFiAXDH2diJC@cluster0.z09uy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
+  'mongodb+srv://varunquanth0001:VhE01HkT89q5u8oC@cluster0.t4jif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
